b4019c16ac340821ac880776deca63946d62c2d8
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-11-02 16:05:18 +0530

    Refactor PDA address generation logic in TypeScript
    - Updated `index.ts` to streamline the PDA generation process using `findProgramAddressSync`.
    - Replaced the previous bump seed loop with a more efficient implementation.
    - Added console logs to display the derived PDA and bump values for better debugging.

--- pda-ts/index.ts
+++ pda-ts/index.ts
@@ -1,17 +1,30 @@
+//-----TS Script to find PDA-----
 import { PublicKey } from "@solana/web3.js";
 
 const programId = new PublicKey("11111111111111111111111111111111");
-const optionalSeed = "solwarrior";
+const seed = "solwarrior";
+const vault_add = new PublicKey("BQuvWWJmjhS2X4jc6G9T2meEHdyzY6RsooTHLMABKeah");
 
-// Loop through all bump seeds (255 down to 0)
-for (let bump = 255; bump >= 0; bump--) {
-  try {
-    const PDA = PublicKey.createProgramAddressSync(
-      [Buffer.from(optionalSeed), Buffer.from([bump])],
-      programId
-    );
-    console.log("bump " + bump + ": " + PDA);
-  } catch (error) {
-    console.log("bump " + bump + ": " + error);
-  }
-}
+//findProgramAddressSync
+//looping
+
+// for (let bump = 255; bump >= 253; bump--) {
+//   try {
+//     const PDA = PublicKey.createProgramAddressSync(
+//       [Buffer.from(seed), Buffer.from([bump])],
+//       programId
+//     );
+
+//     console.log("Bump : ", bump, "PDA : ", PDA.toBase58());
+//   } catch (error) {
+//     console.log("Bump : ", error);
+//   }
+// }
+
+const [pda, bump] = PublicKey.findProgramAddressSync(
+  [Buffer.from(seed), vault_add.toBuffer()],
+  programId
+);
+
+console.log("Pda : ", pda.toBase58()); //4NajcZCzkipVs8SrvtCdN5pVM5KLLNd2aRQVb9mrdXHr
+console.log("Bump : ", bump); //255
