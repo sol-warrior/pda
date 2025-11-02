123b0f4ee654ada89708fd248d422ef88b6d580f
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-11-02 16:04:18 +0530

    Update tests for PDA account initialization and fetching
    - Renamed test suite from `pda-rs` to `pda-account` to reflect the updated program name.
    - Enhanced the initialization test to include user account as a parameter.
    - Added a new test case to fetch and log the PDA account data after initialization.

--- pda-anchor/tests/pda-rs.ts
+++ pda-anchor/tests/pda-rs.ts
@@ -1,16 +1,34 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { PdaRs } from "../target/types/pda_rs";
+import { PdaAccount } from "../target/types/pda_account";
+import { PublicKey } from "@solana/web3.js";
 
-describe("pda-rs", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("pda-account", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.pdaRs as Program<PdaRs>;
+  const program = anchor.workspace.PdaAccount as Program<PdaAccount>;
+  const user = provider.wallet as anchor.Wallet;
+
+  // Derive the PDA address using the seeds specified on the program
+  const [PDA] = PublicKey.findProgramAddressSync(
+    [Buffer.from("solwarrior"), user.publicKey.toBuffer()],
+    program.programId
+  );
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const transactionSignature = await program.methods
+      .initialize()
+      .accounts({
+        user: user.publicKey,
+      })
+      .rpc();
+
+    console.log("Transaction Signature:", transactionSignature);
+  });
+
+  it("Fetch Account", async () => {
+    const pdaAccount = await program.account.dataAccount.fetch(PDA);
+    console.log(JSON.stringify(pdaAccount, null, 2));
   });
 });
