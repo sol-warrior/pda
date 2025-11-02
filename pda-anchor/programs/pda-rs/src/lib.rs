48281dc46fa43142574bf750ad6dc6559def45db
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-11-02 16:04:05 +0530

    Refactor PDA program structure and enhance initialization logic
    - Renamed the program module from `pda_rs` to `pda_account` for clarity.
    - Expanded the `Initialize` struct to include a user signer and a PDA account with proper initialization parameters.
    - Implemented logic in the `initialize` function to set the user and bump values in the PDA account.
    - Introduced a new `DataAccount` struct to store user and bump information with appropriate attributes.

--- pda-anchor/programs/pda-rs/src/lib.rs
+++ pda-anchor/programs/pda-rs/src/lib.rs
@@ -3,14 +3,42 @@ use anchor_lang::prelude::*;
 declare_id!("8pKd7UzCkLS3og9yk97WSGWehSf4AD7cXLi5Bpj8oJPd");
 
 #[program]
-pub mod pda_rs {
+pub mod pda_account {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+
+        let account_data = &mut ctx.accounts.pda;
+        account_data.user = *ctx.accounts.user.key;
+
+        account_data.bump = ctx.bumps.pda;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        seeds= [b"solwarrior", user.key().as_ref()],
+        bump,
+        payer = user,
+        space = 8 + DataAccount::INIT_SPACE
+
+    )]
+    pub pda: Account<'info, DataAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct DataAccount {
+    pub user: Pubkey,
+    pub bump: u8,
+}
